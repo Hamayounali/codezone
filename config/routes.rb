75e61ab142125ead3d51bdedf384c441811fa0af
Hamayoun <humayoonali2@gmail.com>
2022-11-01 16:36:34 +0430

    Blog app - controllers
    Performed Activities
    1. Created Posts and users controllers
    2. Created Posts and users views
    3. Added the required routs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  get '/articles', to: 'articles#index'
+  root 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id/posts/:id', to: 'posts#show'
 end
