6e1deef16075afb8fcf0f908a942a60f8c048537
Hamayoun <humayoonali2@gmail.com>
2022-11-04 23:52:17 +0430

    Blog app - Add forms
    Performed Activites
    1. Created method current_user
    2. Created New post form
    3. Created New Comment form
    4. Created Like form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,32 @@ class PostsController < ApplicationController
     @likes = Post.find(params[:id]).likes
     @comments = Post.find(params[:id]).comments
   end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params.merge(comments_counter: 0, likes_counter: 0, author_id: current_user.id))
+    respond_to do |format|
+      if @post.save
+        format.html do
+          redirect_to "/users/#{@post.user.id}", notice: 'Post was successfully created.'
+        end
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
